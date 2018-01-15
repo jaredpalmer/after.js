84137e1b0d5b27d94fd34ac1f5313cb54163ff0b
Zach Silveira <zach@encryption.io>
2018-01-15 15:26:17 -0500

    Make load initial props a little clearer!

--- packages/after/lib/loadInitialProps.js
+++ packages/after/lib/loadInitialProps.js
@@ -1,23 +1,19 @@
 import { matchPath } from 'react-router-dom';
 
 export default function loadInitialProps(routes, pathname, ctx) {
-  const promises = [];
-  routes.some(route => {
+  return Promise.all(routes.filter(route => {
     const match = matchPath(pathname, route);
-    if (match && route.component.getInitialProps) {
-      promises.push(
-        route.component.load
-          ? route.component
-              .load() // load it as well
-              .then(() =>
-                route.component
-                  .getInitialProps({ match, ...ctx })
-                  .catch(() => {})
-              )
-          : route.component.getInitialProps({ match, ...ctx }).catch(() => {})
-      );
-    }
-    return !!match;
-  });
-  return Promise.all(promises);
+    if (!match || !route.component.getInitialProps) return false
+    return true
+  })
+    .map(route => route.component.load
+      ? route.component
+        .load() // load it as well
+        .then(() =>
+          route.component
+            .getInitialProps({ match, ...ctx })
+            .catch(() => { })
+        )
+      : route.component.getInitialProps({ match, ...ctx }).catch(() => { }))
+  )
 }
