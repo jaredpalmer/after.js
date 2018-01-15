bbe1c6e27d6694fd06ded37f10121bbc6f198f37
Mohsen Azimi <me@azimi.me>
2018-01-15 12:39:59 -0800

    Initial commit

---
+++ examples/typescript/src/_routes.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import asyncComponent from '@jaredpalmer/after/asyncComponent';
+import Home from './Home';
+
+export default [
+  {
+    path: '/',
+    exact: true,
+    component: Home,
+  },
+  {
+    path: '/about',
+    exact: true,
+    component: asyncComponent({
+      loader: () => import('./About'),
+      Placeholder: () => <div>LOADING...</div>,
+    }),
+  },
+];
