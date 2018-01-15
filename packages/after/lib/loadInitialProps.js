63dad7c6938807b01fafcc437197d4de70e5b948
Zach Silveira <zach@encryption.io>
2018-01-15 16:51:16 -0500

    Update loadInitialProps.js

--- packages/after/lib/loadInitialProps.js
+++ packages/after/lib/loadInitialProps.js
@@ -1,19 +1,19 @@
 import { matchPath } from 'react-router-dom';
 
 export default function loadInitialProps(routes, pathname, ctx) {
-  return Promise.all(routes.filter(route => {
     const match = matchPath(pathname, route);
-    if (!match || !route.component.getInitialProps) return false
-    return true
-  })
-    .map(route => route.component.load
-      ? route.component
-        .load() // load it as well
-        .then(() =>
-          route.component
-            .getInitialProps({ match, ...ctx })
-            .catch(() => { })
-        )
-      : route.component.getInitialProps({ match, ...ctx }).catch(() => { }))
+
+  return Promise.all(
+    routes
+      .filter(route => match && route.component.getInitialProps))
+      .map(route => route.component.load
+        ? route.component
+          .load() // load it as well
+          .then(() =>
+            route.component
+              .getInitialProps({ match, ...ctx })
+              .catch(() => { })
+          )
+        : route.component.getInitialProps({ match, ...ctx }).catch(() => { }))
   )
 }
