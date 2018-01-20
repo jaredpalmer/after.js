a3f2d4dbdf6de7664159bd4fe2b3265c7c76429f
Assin <elhammouti.assim@gmail.com>
2018-01-20 14:12:48 +0000

    Remove build directory

--- examples/with-styled-components/build/src/components/Page.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Page = styled.div`
-  text-align: center;
-  padding: 2rem;
-  margin: 0 auto;
-`;
-
-export default Page;
--- examples/with-styled-components/build/src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Logo } from './Logo';
-export { default as Page } from './Page';
