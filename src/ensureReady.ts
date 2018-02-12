5e12f527bd35a9a3a62f9bc4671b740653c3f2ac
Jared Palmer <jared@palmer.net>
2018-02-12 11:08:01 -0500

    Fix #96. Make pathname optional in ensureReady() in TS

--- src/ensureReady.ts
+++ src/ensureReady.ts
@@ -3,7 +3,7 @@ import { matchPath, RouteProps } from 'react-router-dom';
 /**
  * This helps us to make sure all the async code is loaded before rendering.
  */
-export async function ensureReady(routes: RouteProps[], pathname: string) {
+export async function ensureReady(routes: RouteProps[], pathname?: string) {
   await Promise.all(
     routes.map(route => {
       const match = matchPath(pathname || window.location.pathname, route);
