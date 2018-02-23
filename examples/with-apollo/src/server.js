ae14faca80c354f6097315bf235c9e78122e0532
Bruno RZN <bruno.ronzani@oyez.fr>
2018-02-23 19:21:29 +0100

    updated server with custom renderer

--- examples/with-apollo/src/server.js
+++ examples/with-apollo/src/server.js
@@ -1,6 +1,10 @@
+import React from 'react';
 import express from 'express';
 import { render } from '@jaredpalmer/after';
+import { renderToString as rdd } from 'react-dom/server';
+import { ApolloProvider, renderToStringWithData } from 'react-apollo';
 import routes from './routes';
+import createApolloClient from './createApolloClient';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -9,12 +13,20 @@ server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', async (req, res) => {
+    const client = createApolloClient({ ssrMode: true });
+
+    const renderToString = node =>
+      renderToStringWithData(
+        <ApolloProvider client={client}>{node}</ApolloProvider>
+      );
+
     try {
       const html = await render({
         req,
         res,
         routes,
         assets,
+        renderToString,
         // Anything else you add here will be made available
         // within getInitialProps(ctx)
         // e.g a redux store...
