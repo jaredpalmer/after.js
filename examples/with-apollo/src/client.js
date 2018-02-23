3a3778456242f7ec9f4cbd70de608b018f221cc7
Bruno RZN <bruno.ronzani@oyez.fr>
2018-02-23 19:21:50 +0100

    updated client.js

--- examples/with-apollo/src/client.js
+++ examples/with-apollo/src/client.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ensureReady, After } from '@jaredpalmer/after';
+import { ApolloProvider } from 'react-apollo';
 import './client.css';
 import routes from './routes';
+import createApolloClient from './createApolloClient';
+
+const client = createApolloClient({ ssrMode: false });
+
+const renderDOM = !!module.hot ? render : hydrate;
 
 ensureReady(routes).then(data =>
-  hydrate(
-    <BrowserRouter>
-      <After data={data} routes={routes} />
-    </BrowserRouter>,
+  renderDOM(
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <After data={data} routes={routes} />
+      </BrowserRouter>
+    </ApolloProvider>,
     document.getElementById('root')
   )
 );
-
 if (module.hot) {
   module.hot.accept();
 }
