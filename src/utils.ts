faf9202a9bcb89ebd85e698fe44907c4860edff5
Quinn Chaffee <chaffeqa@gmail.com>
2019-04-20 11:01:49 -0400

    Refactor to share logic p2

--- src/utils.ts
+++ src/utils.ts
@@ -19,3 +19,35 @@ export function isAsyncComponent(Component: AsyncRouteableComponent): Component 
 export function isLoadableComponent(Component: AsyncRouteableComponent): Component is AsyncRouteComponentType<any> {
   return (<AsyncRouteComponentType<any>>Component).load !== undefined;
 }
+
+
+/** @public Find route similar to how RR Switch does */
+export function findMatchedRoute(
+  routes: AsyncRouteProps[],
+  pathname: string,
+) {
+  const matchedRoute = routes.find(
+    (route) => !!route.hasOwnProperty("path") && !!matchPath(pathname, route)
+  ) || routes.find(
+    // if no route is found, attempt to find a fallback (not-found) route
+    (route) => !route.hasOwnProperty("path")
+  )
+  // if this is the fallback (not-found) route, `matchPath` errors out if there is no `path`, so we use "*"
+  const match = matchedRoute && matchPath(pathname, matchedRoute.hasOwnProperty("path") ? matchedRoute : { path: "**" }) || undefined
+  
+  return {
+    match,
+    route: matchedRoute,
+  }
+}
+
+/** @public Return promise of loading component if it is an `isLoadableComponent` */
+export function loadRouteComponent(
+  route: AsyncRouteProps,
+) {
+  if (route.component && isLoadableComponent(route.component) && route.component.load) {
+    return route.component.load()
+  } else {
+    return Promise.resolve(route.component || null)
+  }
+}
