b24b23dc22889190ac3f119a625fc19efdbfb71d
Quinn Chaffee <chaffeqa@gmail.com>
2019-04-20 09:51:32 -0400

    RR v5 bug + fallback async support
    Addresses x2 issues:
    1. React-Router v5 introduced a bug where `matchPath` will throw an error if no `path` property is on the route.
    2. If using a "fallback' (not-found) route, the route would not support async loading / `getInitialProps` in the previous implementation

--- src/loadInitialProps.tsx
+++ src/loadInitialProps.tsx
@@ -3,26 +3,24 @@ import { AsyncRouteProps, InitialProps, CtxBase } from './types';
 import { isAsyncComponent } from './utils';
 
 export async function loadInitialProps(routes: AsyncRouteProps[], pathname: string, ctx: CtxBase): Promise<InitialProps> {
-  const promises: Promise<any>[] = [];
-
-  const matchedComponent = routes.find((route: AsyncRouteProps) => {
-    const match = matchPath(pathname, route);
-
-    if (match && route.component && isAsyncComponent(route.component)) {
-      const component = route.component;
-
-      promises.push(
-        component.load
-          ? component.load().then(() => component.getInitialProps({ match, ...ctx }))
-          : component.getInitialProps({ match, ...ctx })
-      );
-    }
-
-    return !!match;
-  });
+  const matchedRoute = routes.find(
+    (route) => !!route.hasOwnProperty("path") && !!matchPath(pathname, route)
+  ) || (
+    // if no route is found, attempt to find a fallback (not-found) route
+    (route) => !route.hasOwnProperty("path")
+  )
+  // if this is the fallback (not-found) route, `matchPath` errors out if there is no `path`, so we use "*"
+  const match = matchedRoute && matchPath(pathname, matchedRoute.path || "*")
+  
+  const matchedComponent = matchedRoute && matchedRoute.component || undefined
+  const initialPropsData = matchedComponent && isAsyncComponent(matchedComponent) ? await (
+    component.load
+      ? component.load().then(() => component.getInitialProps({ match, ...ctx }))
+      : component.getInitialProps({ match, ...ctx })
+  ) : undefined
   
   return {
     match: matchedComponent,
-    data: (await Promise.all(promises))[0]
+    data: initialPropsData,
   };
 }
