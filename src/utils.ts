ad2a15ce8eb31722278a87f8031fad5ed23ea33c
Nikita Nafranets <eddimensi@gmail.com>
2019-07-31 16:31:37 +0300

    remove check promise helper

--- src/utils.ts
+++ src/utils.ts
@@ -6,10 +6,6 @@ export const isFunction = (obj: any) => 'function' === typeof obj;
 /** @private is the given object an Object? */
 export const isObject = (obj: any) => obj !== null && typeof obj === 'object';
 
-/** @private is the given object/value a promise? */
-export const isPromise = (value: any): boolean =>
-  isObject(value) && isFunction(value.then);
-
 /** @private Guard cluase to narrow the AsyncRouteableComponent union type on getInitialProps */
 export function isAsyncComponent(Component: AsyncRouteableComponent): Component is AsyncRouteComponentType<any> {
   return (<AsyncRouteComponentType<any>>Component).getInitialProps !== undefined;
