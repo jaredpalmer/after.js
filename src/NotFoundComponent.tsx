446a849f90f82f24691d0723d9cfa7cb637703f7
nima <nimaarefi@outlook.com>
2019-08-13 01:36:40 +0430

    add static data variable for NotFound component

--- src/NotFoundComponent.tsx
+++ src/NotFoundComponent.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Route } from "react-router-dom"
 
-function NotFound() {
-	return (
-		<Route
-			render={({ staticContext }) => {
-				if (staticContext) staticContext.statusCode = 404;
-				return <div>The Page You Were Looking For Was Not Found</div>;
-			}}
-		/>
-	)
+class NotFound extends React.Component {
+
+	// just for test purpose
+	static data = `The Page You Were Looking For Was Not Found`
+
+	render() {
+		return (
+			<Route
+				render={({ staticContext }) => {
+					if (staticContext) staticContext.statusCode = 404;
+					return <div>The Page You Were Looking For Was Not Found</div>;
+				}}
+			/>
+		)
+	}
 }
 
 export default NotFound;
