a2e6fb2780b53a4806b4fd2e229c34684e8fedef
nima <nimaarefi@outlook.com>
2019-08-13 01:37:52 +0430

    add tests for error pages

---
+++ src/test/components/AsyncNotFoundComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AsyncNotFoundComponent extends React.Component {
+  static displayName = "AsyncNotFoundComponent";
+
+  static async getInitialProps({ req, res, match, history, location, ...ctx }) {
+    return await Promise.resolve({ statusCode: 404 });
+  }
+
+  render() {
+    return <h1>Page Not Found</h1>;
+  }
+}
+
+export default AsyncNotFoundComponent;
