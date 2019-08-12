b4613c3076b908aaf8969a67553bd0a35b09428d
nima <nimaarefi@outlook.com>
2019-08-12 22:16:19 +0430

    new utils for routes

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
-import { AsyncRouteableComponent, AsyncRouteComponentType } from "./types";
+import { AsyncRouteableComponent, AsyncRouteComponentType, AsyncRouteProps,  } from "./types";
+import NotFoundComponent from "./NotFoundComponent"
 
 /** @private is the given object a Function? */
 export const isFunction = (obj: any) => 'function' === typeof obj;
@@ -18,4 +19,20 @@ export function isAsyncComponent(Component: AsyncRouteableComponent): Component 
 /** @private Guard cluase to narrow the AsyncRouteableComponent union type on load */
 export function isLoadableComponent(Component: AsyncRouteableComponent): Component is AsyncRouteComponentType<any> {
   return (<AsyncRouteComponentType<any>>Component).load !== undefined;
+}
+
+/** @private is given routes have 404 page?  */
+export function is404ComponentAvailable(routes: AsyncRouteProps<any>[]): AsyncRouteProps<any> | false {
+  return routes.find(route => ["**", "*", undefined].includes(route.path)) || false
+}
+
+/** @private Returns 404Component from given routes if component was not avaliable returns default 404component */
+export function get404Component(routes: AsyncRouteProps<any>[]): AsyncRouteableComponent<any> {
+	const match = is404ComponentAvailable(routes)
+  return match ? match.component : NotFoundComponent 
+}
+
+/** @private Checks if 404Component is in routes, if it's not available add default 404 component */
+export function getAllRoutes(routes: AsyncRouteProps<any>[]): AsyncRouteProps<any>[] {
+  return is404ComponentAvailable(routes) ? routes : [...routes, { component: NotFoundComponent }]
 }
