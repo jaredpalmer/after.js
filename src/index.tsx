0d1021aab56923b7b87f0d65fad45e38f6c53006
nima <nimaarefi@outlook.com>
2019-09-08 11:51:14 +0430

    remove transformRoutes module

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 export * from './After';
 export * from './asyncComponent';
 export * from './Document';
-export * from './transformRoutes';
 export * from './loadInitialProps';
 export * from './render';
 export * from './types';
