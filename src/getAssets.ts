eb3b7e18c2347b26ecfeb0cc2a0461a8eb4999f3
nima <nimaarefi@outlook.com>
2019-09-09 19:00:20 +0430

    show useful error message

--- src/getAssets.ts
+++ src/getAssets.ts
@@ -7,33 +7,27 @@ export function getAssets({ route, manifest }: getAssets) {
   let scripts: string[] = [];
   let styles: string[] = [];
 
-  // if component was isLoadableComponent and chunkName was undefined
+  // if component was LoadableComponent and chunkName was undefined
   // print an error message to console so Developer can fix it
   // @todo add link to documentation or show more useful error message
-  if (process.env.NODE_ENV !== 'production') {
-    if (isLoadableComponent(route.component) && route.chunkName === undefined) {
-      logger.error(
-        `all async routes must have a "chunkName" property with value of /* webpackChunkName: "MyChunkName" */ ${JSON.stringify(
-          { chunkName: 'MyChunkName', ...route },
-          null,
-          2
-        )}`
-      );
-      throw new Error();
+  if (isLoadableComponent(route.component) && route.chunkName === undefined) {
+    logger.error(
+      `all async routes must have a chunkName property with value of /* webpackChunkName: "MyChunkName" */ check your routes config or use babel-plugin-after-async-component`
+		);
+		throw new Error()
+  }
+
+  // if component was LoadableComponent and had a chunkName
+  // then we look for it's assets from manifest
+  if (isLoadableComponent(route.component) && route.chunkName) {
+    const { chunkName } = route;
+
+    if (manifest[chunkName] && manifest[chunkName].js) {
+      scripts = manifest[chunkName].js;
     }
 
-    // if component was LoadableComponent and had a chunkName
-    // then we look for it's assets from manifest
-    if (isLoadableComponent(route.component) && route.chunkName) {
-      const { chunkName } = route;
-
-      if (manifest[chunkName] && manifest[chunkName].js) {
-        scripts = manifest[chunkName].js;
-      }
-
-      if (manifest[chunkName] && manifest[chunkName].css) {
-        styles = manifest[chunkName].css;
-      }
+    if (manifest[chunkName] && manifest[chunkName].css) {
+      styles = manifest[chunkName].css;
     }
   }
 
