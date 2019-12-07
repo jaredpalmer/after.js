f4ddf6ff4a9c60712211538c9a937499f55b7089
Nima Arefi <nimaarefi@outlook.com>
2019-12-07 14:46:35 +0330

    remove unsued files

--- document.js
+++
@@ -1,3 +0,0 @@
-module.exports = require('./build/cjs/Document').Document;
-exports.AfterRoot = require('./build/cjs/Document').AfterRoot;
-exports.AfterData = require('./build/cjs/Document').AfterData;
