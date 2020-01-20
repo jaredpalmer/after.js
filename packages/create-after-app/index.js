192bfb7f97c385c3e66f6b347adaefd2959d4fdf
Nima Arefi <nimaarefi@outlook.com>
2020-01-20 12:46:45 +0330

    change create-razzle-app to create-after-app

---
+++ packages/create-after-app/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const createAfter = require('./lib');
+const messages = require('./lib/messages');
+
+module.exports = {
+  messages: messages,
+  createAfterApp: createAfter,
+};
---
+++ packages/create-after-app/lib/utils/load-example.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const exec = require('execa');
+const Promise = require('promise');
+const output = require('./output');
+
+module.exports = function loadExample(opts) {
+  const projectName = opts.projectName;
+  const example = opts.example;
+  const cmds = [
+    `mkdir -p ${projectName}`,
+    `curl https://codeload.github.com/jaredpalmer/after.js/tar.gz/master | tar -xz -C ${projectName} --strip=3 after.js-master/examples/${example}`,
+  ];
+
+  const stopExampleSpinner = output.wait(
+    `Downloading files for ${output.cmd(example)} example`
+  );
+  const cmdPromises = cmds.map(function(cmd) {
+    return exec.shell(cmd);
+  });
+
+  return Promise.all(cmdPromises).then(function() {
+    stopExampleSpinner();
+    output.success(
+      `Downloaded ${output.cmd(example)} files for ${output.cmd(projectName)}`
+    );
+  });
+};
