2611083929b00b9175b5893b50de919afb5d17a8
Nima Arefi <nimaarefi@outlook.com>
2020-07-12 02:28:44 +0430

    examples: Update with-prerender example

--- examples/with-prerender/src/server.js
+++ examples/with-prerender/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { render as afterRender } from '@jaredpalmer/after';
+import { render as afterRender, renderStatic } from '@jaredpalmer/after';
 import routes from './routes';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
@@ -7,17 +7,16 @@ const chunks = require(process.env.RAZZLE_CHUNKS_MANIFEST);
 
 export const render = async (req, res) => {
   try {
-    const html = await afterRender({
+    const { html, data } = await renderStatic({
       req,
       res,
       routes,
       assets,
       chunks,
     });
-    res.send(html);
+    return { html, data };
   } catch (error) {
-    console.error(error);
-    res.json({ message: error.message, stack: error.stack });
+    return { error: error.message };
   }
 };
 
@@ -25,6 +24,20 @@ const server = express();
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', render);
+  .get('/*', async (req, res) => {
+    try {
+      const html = await afterRender({
+        req,
+        res,
+        routes,
+        assets,
+        chunks,
+      });
+      res.send(html);
+    } catch (error) {
+      console.error(error);
+      res.json({ message: error.message, stack: error.stack });
+    }
+  });
 
 export default server;
