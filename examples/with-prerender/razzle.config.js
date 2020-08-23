7b223981bd297baf08beba12bea1a5597d898c11
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-08-23 18:31:30 +0200

    fix: use new camelCased razzle options

--- examples/with-prerender/razzle.config.js
+++ examples/with-prerender/razzle.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   experimental: {
-    static_export: {
-      window_routes_variable: 'AFTER_STATIC_ROUTES',
-      window_routes_data_variable: 'AFTER_STATIC_DATA_ROUTES',
+    staticExport: {
+      windowRoutesVariable: 'AFTER_STATIC_ROUTES',
+      windowRoutesDataVariable: 'AFTER_STATIC_DATA_ROUTES',
     },
   },
 };
