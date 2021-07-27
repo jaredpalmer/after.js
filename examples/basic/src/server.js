86b2b7d81a9c1ed04f74f094807b67e486ae8d24
Anthony M Powers <tehraven@gmail.com>
2021-07-26 20:58:16 -0700

    Now with more razzle

--- examples/basic/src/server.js
+++ examples/basic/src/server.js
@@ -1,22 +1,21 @@
-import express from 'express';
-import { render } from '@jaredpalmer/after';
-import routes from './routes';
+import express from "express";
+import { render } from "@jaredpalmer/after";
+import routes from "./routes";
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
-const chunks = require(process.env.RAZZLE_CHUNKS_MANIFEST);
 
 const server = express();
 server
-  .disable('x-powered-by')
+  .disable("x-powered-by")
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', async (req, res) => {
+  .get("/*", async (req, res) => {
     try {
       const html = await render({
         req,
         res,
         routes,
         assets,
-        chunks,
+        chunks: assets,
       });
       res.send(html);
     } catch (error) {
