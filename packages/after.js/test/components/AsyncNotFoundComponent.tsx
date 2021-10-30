47b24ba57d1818704aff89c5580650ae48ad7d1c
Nima Arefi <nimaarefi@outlook.com>
2021-10-31 01:35:11 +0330

    feat: move tests outside src directory

--- packages/after.js/test/components/AsyncNotFoundComponent.tsx
+++ packages/after.js/test/components/AsyncNotFoundComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class AsyncNotFoundComponent extends React.Component {
   static displayName = 'AsyncNotFoundComponent';
-  static data = "The Page You Were Looking For Was Not Found"
+  static data = 'The Page You Were Looking For Was Not Found';
 
   static async getInitialProps() {
     return await Promise.resolve({ statusCode: 404 });
